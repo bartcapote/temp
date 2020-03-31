8950a037b292af7c4dc88af03e067827e3f4feb8
bartcapote <gauclee@gmail.com>
2020-03-31 12:57:21 +0200

    Add toggle_player()

--- battleship.py
+++ battleship.py
@@ -9,3 +9,11 @@ def init_boards():
               [0, 0, 0, 0, 0]]
     board2 = copy.deepcopy(board1)
     return board1, board2
+
+
+def toggle_player(player):
+    if player == 1:
+        player = 2
+    else:
+        player = 1
+    return player
