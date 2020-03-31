b0d2055ea7df01bdb8e30745cac642d027b7a50f
bartcapote <gauclee@gmail.com>
2020-03-31 12:55:52 +0200

    Add init_boards(), import copy

--- battleship.py
+++ battleship.py
@@ -1,11 +1,11 @@
-def init_board():
-    board = [[O, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0]]
+import copy
 
 
-def print_board():
-    board = []
-    
+def init_boards():
+    board1 = [[0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0]]
+    board2 = copy.deepcopy(board1)
+    return board1, board2
