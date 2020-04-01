6d47f09387f5a9c0eb841bc7402a57ff36bc1fea
BBeata85 <beata.hajto@gmail.com>
2020-04-01 14:00:52 +0200

    Add first modifications after merge

--- battleship.py
+++ battleship.py
@@ -2,25 +2,25 @@ import copy
 import time
 import os
 import random
-import tkinter as tk
+# import tkinter as tk
 
 
 def init_boards():
-    board1 = [[0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 0]]
+    board1 = [['0', '0', '0', '0', '0'],
+              ['0', '0', '0', '0', '0'],
+              ['0', '0', '0', '0', '0'],
+              ['0', '0', '0', '0', '0'],
+              ['0', '0', '0', '0', '0']]
     board2 = copy.deepcopy(board1)
     return board1, board2
 
 
 def print_board(board):
     row_headers = "ABCDE"
-    col_headers = " 12345"
+    col_headers = "12345"
     for row in range(6):
         if row == 0:
-            print(" " + "".join(col_headers))
+            print("  " + "".join(col_headers))
         else:
             print(row_headers[row - 1] + " " + "".join(board[row - 1]))
 
@@ -33,18 +33,9 @@ def toggle_player(player):
     return player
 
 
-def get_input():	
-    col = input("col (A to E):")
-    while col not in "ABCDE":
-        print("That column is wrong! It should be A, B, C, D or E")
-        col = input("column (A to E):")
-
-    row = input("row (1 to 5):")
-    while row not in "12345":
-        print("That row is wrong! it should be 1, 2, 3, 4 or 5")
-        row = input("row (1 to 5):")
-
-    return row, col
+def get_input():
+    user_input = input("Input: ")
+    return user_input
 
 
 def wrong_input_message(board):
@@ -54,17 +45,25 @@ def wrong_input_message(board):
     # print_board(board)  -  COMMENT OUT UNTIL print_board() IS DEFINED
 
 
-def check_input(board, user_input):
+def convert_to_coordinates(user_input):
     alphabet_reference = 'ABCDEFGHIKLMNOPQRSTUVWXYZ'
+    row_index = alphabet_reference.find(user_input[0].upper())
+    col_index = int(user_input[1]) - 1
+    return row_index, col_index
+
+
+def check_input_format(board, user_input):
     if len(user_input) == 2 and user_input[0].isalpha() and user_input[1].isnumeric():
-        row_index = alphabet_reference.find(user_input[0].upper())
-        col_index = int(user_input[1]) - 1
-        board_height = len(board)
-        if row_index < board_height and col_index < board_height:
-            return True
-        else:
-            wrong_input_message(board)
-            return False
+        return True
+    else:
+        wrong_input_message(board)
+        return False
+
+
+def check_if_coordinates_in_board(board, row_index, col_index):
+    board_height = len(board)
+    if row_index < board_height and col_index < board_height:
+        return True
     else:
         wrong_input_message(board)
         return False
@@ -113,34 +112,34 @@ def print_miss_message():
 
 def print_sunk_message():
     if row_index == "X" and col_index == "X":
-    print('Ship sunk!')
+        print('Ship sunk!')
 
 
-def display_waiting_screen():	
-    sleep(2) 
-    root.destroy()
-
-    root = tk.Tk()
-    root.title("Battleship")
-
-    label = tk.Label(root, text="Wait for input.")
-    label.pack()
-
-    root.after(200, task)
-    root.mainloop()
-
-    print("You can start a game now.")
+# def display_waiting_screen():	
+#     sleep(2) 
+#     root.destroy()
+# 
+#     root = tk.Tk()
+#     root.title("Battleship")
+# 
+#     label = tk.Label(root, text="Wait for input.")
+#     label.pack()
+# 
+#     root.after(200, task)
+#     root.mainloop()
+# 
+#     print("You can start a game now.")
 
 
 def battleship_game():
     max_ships = 8
     board1 = init_boards()[0]
     while check_ships_left(board1, max_ships):
-        # board2 = init_boards()[1]         |
-        # print_board(board1)               |
-        # user_input = get_input(board1)    |
-        # place_ship(board1, user_input)    |
-        # print_board(board1)               | COMMENT OUT UNTIL REFERENCES DEFINED
+        board2 = init_boards()[1]
+        print_board(board1)
+        user_input = get_input(board1)
+        place_ship(board1, user_input)       
+        print_board(board1)
         pass
 
 
