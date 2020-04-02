66bdd57b35c06938f5e5479b5ec6002038a75d7e
bartcapote <gauclee@gmail.com>
2020-04-02 17:30:51 +0200

    Make init_boards() better

--- battleship.py
+++ battleship.py
@@ -4,12 +4,10 @@ import os
 import random
 
 
-def init_boards():
-    board1 = [['~', '~', '~', '~', '~'],
-              ['~', '~', '~', '~', '~'],
-              ['~', '~', '~', '~', '~'],
-              ['~', '~', '~', '~', '~'],
-              ['~', '~', '~', '~', '~']]
+def init_boards(height):
+    board1 = []
+    for i in range(height):
+        board1.append(['\033[36m~\033[0m'] * height)
     board2 = copy.deepcopy(board1)
     return board1, board2
 
