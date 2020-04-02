764437ecd3acf5ccede7a790775c909a39a36a3e
bartcapote <gauclee@gmail.com>
2020-04-02 09:48:52 +0200

    Mega commit :(
    Several functions specific to ship size were separated.
    Added a few fixes tto the existing functions.
    Little time. Sorry for binge commit.

--- battleship.py
+++ battleship.py
@@ -2,15 +2,14 @@ import copy
 import time
 import os
 import random
-# import tkinter as tk
 
 
 def init_boards():
-    board1 = [['0', '0', '0', '0', '0'],
-              ['0', '0', '0', '0', '0'],
-              ['0', '0', '0', '0', '0'],
-              ['0', '0', '0', '0', '0'],
-              ['0', '0', '0', '0', '0']]
+    board1 = [['~', '~', '~', '~', '~'],
+              ['~', '~', '~', '~', '~'],
+              ['~', '~', '~', '~', '~'],
+              ['~', '~', '~', '~', '~'],
+              ['~', '~', '~', '~', '~']]
     board2 = copy.deepcopy(board1)
     return board1, board2
 
@@ -41,19 +40,31 @@ def get_input():
 
 def wrong_input_message(board):
     print('\033[1m\033[31mWrong input! Try again.\033[0m')
-    time.sleep(0.8)
-    os.system('clear')
-    # print_board(board)  -  COMMENT OUT UNTIL print_board() IS DEFINED
+    time.sleep(0.5)
+    print_board(board)
 
 
 def convert_to_coordinates(user_input):
     alphabet_reference = 'ABCDEFGHIKLMNOPQRSTUVWXYZ'
     row_index = alphabet_reference.find(user_input[0].upper())
     col_index = int(user_input[1]) - 1
+    if len(user_input) > 2:
+        direction = user_input[2]
+        return row_index, col_index, direction
     return row_index, col_index
 
 
-def check_input_format(board, user_input):
+def check_input_format_big_ship(board, user_input):
+    if (len(user_input) == 3 and user_input[0].isalpha()
+       and user_input[1].isnumeric()
+       and (user_input[2] == '|' or user_input[2] == '-')):
+        return True
+    else:
+        wrong_input_message(board)
+        return False
+
+
+def check_input_format_small_ship(board, user_input):
     if len(user_input) == 2 and user_input[0].isalpha() and user_input[1].isnumeric():
         return True
     else:
@@ -61,7 +72,23 @@ def check_input_format(board, user_input):
         return False
 
 
-def check_if_coordinates_in_board(board, row_index, col_index):
+def coordinates_in_board_for_big_ship(board, row_index, col_index, direction):
+    board_height = len(board)
+    if direction == '|':
+        if row_index < board_height - 1 and col_index < board_height:
+            return True
+        else:
+            wrong_input_message(board)
+            return False
+    if direction == '-':
+        if row_index < board_height and col_index < board_height - 1:
+            return True
+        else:
+            wrong_input_message(board)
+            return False
+
+
+def coordinates_in_board_for_small_ship(board, row_index, col_index):
     board_height = len(board)
     if row_index < board_height and col_index < board_height:
         return True
@@ -70,17 +97,65 @@ def check_if_coordinates_in_board(board, row_index, col_index):
         return False
 
 
+def move_not_touching_ships(board, row_index, col_index):
+    index_range = len(board) - 1
+    zero_case = False
+    if board[row_index][col_index].isnumeric():
+        return False
+    if row_index == 0:
+        row_index += 1
+        zero_case = True
+    if row_index - 1 <= index_range:
+        if board[row_index - 1][col_index].isnumeric():
+            return False
+    if zero_case:
+        row_index -= 1
+        zero_case = False
+    if row_index + 1 <= index_range:
+        if board[row_index + 1][col_index].isnumeric():
+            return False
+    if col_index == 0:
+        col_index += 1
+        zero_case = True
+    if col_index - 1 <= index_range:
+        if board[row_index][col_index - 1].isnumeric():
+            return False
+    if zero_case:
+        col_index -= 1
+    if col_index + 1 <= index_range:
+        if board[row_index][col_index + 1].isnumeric():
+            return False
+    return True
+
+
+def check_placement(board, row_index, col_index):
+    for row in board:
+        for cell in row:
+            pass
+
+
 def check_ships_left(board, max_ships):
     ship_counter = 0
     for row in board:
         for cell in row:
-            ship_counter += 1 if cell == 'X' else 0
+            if cell.isnumeric():
+                ship_counter += 1
     return True if ship_counter < max_ships else False
 
 
-def place_ship(board, row_index, col_index):
-    board[row_index][col_index] = 'X'
+def place_small_ship(board, row_index, col_index, ship_name):
+    board[row_index][col_index] = str(ship_name)
     return board
+
+
+def place_big_ship(board, row_index, col_index, direction, ship_name):
+    ship_name = str(ship_name)
+    if direction == '-':
+        board[row_index][col_index] = ship_name
+        board[row_index][col_index + 1] = ship_name
+    if direction == '|':
+        board[row_index][col_index] = ship_name
+        board[row_index + 1][col_index] = ship_name
 
 
 def print_player_move_message(player):
@@ -108,34 +183,51 @@ def print_hit_message():
 #         print('Ship sunk!')
 
 
-# def display_waiting_screen():
-#     sleep(2)
-#     root.destroy()
-#
-#     root = tk.Tk()
-#     root.title("Battleship")
-#
-#     label = tk.Label(root, text="Wait for input.")
-#     label.pack()
-#
-#     root.after(200, task)
-#     root.mainloop()
-#
-#     print("You can start a game now.")
-
-
 def battleship_game():
-    max_ships = 8
     board1 = init_boards()[0]
+    ship_name = 0
+    # BIG SHIP:
+    max_ships = 4
     while check_ships_left(board1, max_ships):
         print_board(board1)
         user_input = get_input()
-        if check_input_format(board1, user_input):
+        if check_input_format_big_ship(board1, user_input):
             move_coordinates = convert_to_coordinates(user_input)
             row_index = move_coordinates[0]
             col_index = move_coordinates[1]
-            if check_if_coordinates_in_board(board1, row_index, col_index):
-                board1 = place_ship(board1, row_index, col_index)
+            direction = user_input[2]
+            if (coordinates_in_board_for_big_ship(board1, row_index, col_index, direction)
+               and move_not_touching_ships(board1, row_index, col_index)):
+                if direction == '-':
+                    if move_not_touching_ships(board1, row_index, col_index + 1):
+                        ship_name += 1
+                        place_big_ship(board1, row_index, col_index, direction, ship_name)
+                    else:
+                        wrong_input_message(board1)
+                elif direction == '|':
+                    if move_not_touching_ships(board1, row_index + 1, col_index):
+                        ship_name += 1
+                        place_big_ship(board1, row_index, col_index, direction, ship_name)
+                    else:
+                        wrong_input_message(board1)
+            else:
+                wrong_input_message(board1)
+            print_board(board1)
+    # SMALL SHIP:
+    max_ships = 7
+    while check_ships_left(board1, max_ships):
+        user_input = get_input()
+        if check_input_format_small_ship(board1, user_input):
+            move_coordinates = convert_to_coordinates(user_input)
+            row_index = move_coordinates[0]
+            col_index = move_coordinates[1]
+            if (coordinates_in_board_for_small_ship(board1, row_index, col_index)
+               and move_not_touching_ships(board1, row_index, col_index)):
+                ship_name += 1
+                place_small_ship(board1, row_index, col_index, ship_name)
+                print_board(board1)
+            else:
+                wrong_input_message(board1)
     print_board(board1)
 
 
